7599cd13cf51a459a6172577b7c67635a8154126
antonbashlovka <anton.bashlovka@gmail.com>
2024-06-07 15:50:16 +0500

    Added input strings

--- Itogovaya_java/src/Shop.java
+++
@@ -1,9 +0,0 @@
-public class Shop {
-    
-    private int id;
-    private String name;
-    private int amount;
-    private int weight;
-
-
-}
--- Itogovaya_java/src/Toy.java
+++ Itogovaya_java/src/Toy.java
@@ -1,8 +1,47 @@
-public class Toy {
 
-    private int id;
+
+public class Toy  {
+
+    private String id;
     private String name;
-    private int amount;
-    private int weight;
-    
+    private String weight; 
+
+    public Toy(String id, String name, String weight) {
+        this.id = id;
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public void Get(){
+
+    }
+
+    public void Put(){
+        
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+  
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
 }
